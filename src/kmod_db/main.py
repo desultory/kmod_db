a96f3398b8e7da678196c94ad2a96b24c8e60417
Zen <z@pyl.onl>
2025-08-16 14:27:09 -0500

    use git sources for pyproject, add --blkdev arg
    Signed-off-by: Zen <z@pyl.onl>

--- src/kmod_db/main.py
+++ src/kmod_db/main.py
@@ -8,12 +8,14 @@ def main():
     arguments = [{"flags": ["--acpi"], "help": "Detect ACPI devices", "action": "store_true"},
                  {"flags": ["--pci"], "help": "Detect PCI devices", "action": "store_true"},
                  {"flags": ["--dmi"], "help": "Detect DMI devices", "action": "store_true"},
+                 {"flags": ["--blkdev"], "help": "Get block device kmods"}
                  ]
 
     kwargs = get_kwargs("kmod_db", "kernel module database", arguments=arguments)
     acpi = kwargs.pop("acpi", False)
     pci = kwargs.pop("pci", False)
     dmi = kwargs.pop("dmi", False)
+    blockdev = kwargs.pop("blkdev", None)
 
     db = KmodDB(**kwargs)
 
@@ -23,4 +25,6 @@ def main():
         print(f"PCI Kmods: {db.detect_pci_kmods()}")
     if dmi:
         print(f"DMI Kmods: {db.detect_dmi_kmods()}")
+    if blockdev:
+        print(f"Block device '{blockdev}' kmods: {db.get_blkdev_kmods(blockdev)}")
 
